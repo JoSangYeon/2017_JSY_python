6f65d022201a6011440da31f14d42c3d276f4489
KingGodGeneralJSY <whtkddus98@kookmin.ac.kr>
2017-05-30 18:53:23 +0900

    Update 한글.py

--- my_modules/한글.py
+++ my_modules/한글.py
@@ -1,3 +1,5 @@
+from 한글 import *
+
 def 출력(string):
     return print(string)
 def 만약():
@@ -8,3 +10,6 @@ def 조건반복():
     return 0
 def 가져와라(module):
     import module
+def 받아라(string):
+    retrun input(string)
+    
